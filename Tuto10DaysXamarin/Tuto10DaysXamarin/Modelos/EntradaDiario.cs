f94b1f54d656e7182e5ab11a253211aea3908711
Jose Fco <josefranciscosc@gmail.com>
2019-12-21 18:33:38 +0100

    Dia 4 completado.

---
+++ Tuto10DaysXamarin/Tuto10DaysXamarin/Modelos/EntradaDiario.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuto10DaysXamarin.Modelos
+{
+    class EntradaDiario
+    {
+        [PrimaryKey, AutoIncrement] //Añadir using SQLite;
+        public int id { get; set; }
+        [MaxLength(50)]
+        public string titulo { get; set; }
+        public string contenido { get; set; }
+        public DateTime fechaCreacion { get; set; }
+    }
+}
