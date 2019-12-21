c70a4d32c9ecada93b3a9d18f4e884bb7ad2e29a
Jose Fco <josefranciscosc@gmail.com>
2019-12-21 18:20:17 +0100

    Dia 4 completado.

--- Tuto10DaysXamarin/Tuto10DaysXamarin/MainPage.xaml.cs
+++ Tuto10DaysXamarin/Tuto10DaysXamarin/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Tuto10DaysXamarin.Modelos;
+using SQLite;
 
 namespace Tuto10DaysXamarin
 {
@@ -80,8 +82,35 @@ namespace Tuto10DaysXamarin
          */
          public void botonGuardar_Click(object sender, System.EventArgs e)
         {
-            entradaTitulo.Text = string.Empty;
-            editorCuerpo.Text = string.Empty;
+            // Necesita ser añadido using Tuto10DaysXamarin.Modelos;
+            EntradaDiario nuevaEntradaDiario = new EntradaDiario()
+            {
+                titulo = entradaTitulo.Text,
+                contenido = editorCuerpo.Text,
+                fechaCreacion = DateTime.Now
+            };
+
+            int itemsInsertados = 0;
+            // Necesita se añadido using SQLite;
+            using (SQLiteConnection conex = new SQLiteConnection(App.RutaBaseDatos))
+            {
+                conex.CreateTable<EntradaDiario>();
+                itemsInsertados = conex.Insert(nuevaEntradaDiario);
+            }
+            // Aquí conex ha sido liberada y, por lo tanto, cerrada
+
+            if ( itemsInsertados > 0)
+            {
+                entradaTitulo.Text = string.Empty;
+                editorCuerpo.Text = string.Empty;
+            }
+            else
+            {
+                DisplayAlert("Error", "Hubo un error al guardar al nueva " +
+                    "entrada del diario, por favor prueba de nuevo",
+                    "Aceptar");
+            }
+
         }
     }
 }
